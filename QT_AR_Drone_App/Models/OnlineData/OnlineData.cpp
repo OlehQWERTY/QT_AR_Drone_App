7b5ffac8ccab8e440ae0bb08ad65b003f4a91dfd
Oleh Hordiichenko <oleggordiichenko@gmail.com>
2017-12-08 15:51:27 +0100

    Temp val transformation to uchar color[3] problem!

--- QT_AR_Drone_App/Models/OnlineData/OnlineData.cpp
+++ QT_AR_Drone_App/Models/OnlineData/OnlineData.cpp
@@ -23,29 +23,28 @@ void OnlineData::sensorValToColor(int &val, const int &minTemperature, const int
 
 
     int delta = (val - minTemperature) * coefficient;
-    qDebug() << "delta: " << delta;
     float step = delta / coefficient / 255.0; // convert to uchar
-    qDebug() << "step: " << step;
     unsigned char tempRelativeTemperature = (val - minTemperature * coefficient) / coefficient * step;
     qDebug() << "tempRelativeTemperature: " << tempRelativeTemperature;
 
     if(tempRelativeTemperature < 127)
     {
-        pGeoPoint->color[0] = (tempRelativeTemperature + 1)/127.0;
-        pGeoPoint->color[1] = 1.0;
-        pGeoPoint->color[2] = 1.0;
+        pGeoPoint->color[0] = (tempRelativeTemperature + 1)/127.0 * 255;
+        pGeoPoint->color[1] = 1.0 * 255;
+        pGeoPoint->color[2] = 1.0 * 255;
         //glColor4f((color + 1)/127.0, 1.0, 0, 1.0);
         //qDebug() << (tempRelativeTemperature);
     }
     else
     {
-        pGeoPoint->color[0] = 1.0;
-        pGeoPoint->color[1] = 1.0 - (tempRelativeTemperature + 1)/256.0; // deffence (255 + 1)/255 > 1 -> 1 - 1.001 = minus number -> error
-        pGeoPoint->color[2] = 1.0;
+        pGeoPoint->color[0] = 1.0 * 255;
+        pGeoPoint->color[1] = (1.0 - (tempRelativeTemperature + 1)/256.0) * 255; // deffence (255 + 1)/255 > 1 -> 1 - 1.001 = minus number -> error
+        pGeoPoint->color[2] = 1.0 * 255;
         //glColor4f(1.0, 1.0 - (color + 1)/255.0, 0, 1.0);
 //        qDebug() << (tempRelativeTemperature);
     }
-    //std::cout << tempRelativeTemperature << std::endl; // test
+//    qDebug() << "Color: r " << pGeoPoint->color[0] << "Color: g " << pGeoPoint->color[1] <<
+//                "Color: b " << pGeoPoint->color[2];
 }
 
 GeoPointValues *OnlineData::getOnlineData()
@@ -56,7 +55,7 @@ GeoPointValues *OnlineData::getOnlineData()
     // color val (char[3])
     int tempSensorVal = TcpServer.getLastData(); // QByteArray return TcpServer -> TcpServer.getLastData().toInt(&ok)
 
-    sensorValToColor(tempSensorVal, 20, 40); // correct according to sensor type
+    sensorValToColor(tempSensorVal, 20, 40); // correct according to sensor type and temperature
     qDebug() << "OnlineData: " << tempSensorVal;
 
     return pGeoPoint;
